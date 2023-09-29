2823f10f944c0f7a47ad10a0dd75a4fcb3658b4f
hanna-joo <wnehfehf@gmail.com>
2023-09-30 01:28:39 +0900

    wip: combine naver crawling and processing to one

---
+++ data_collect/naver_scraping.py
@@ -0,0 +1,82 @@
+# pip install selenium
+# pip install webdriver-manager
+# pip install beautifulsoup4
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.chrome.options import Options
+from time import sleep
+from datetime import datetime
+from bs4 import BeautifulSoup
+import re
+import polars as pl
+
+
+# css 찾을때 까지 최대 num초 대기
+def time_wait(sec, selector):
+    try:
+        wait = WebDriverWait(driver, sec).until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, selector)))
+    except:
+        print(selector, '태그를 찾기 못함')
+        driver.quit()
+    return wait
+
+
+# frame 변경 메소드
+def switch_frame(frame):
+    driver.switch_to.default_content()  # frame 초기화
+    driver.switch_to.frame(frame)  # frame 변경
+
+
+# 페이지 스크롤 다운
+def page_down(num):
+    body = driver.find_element(By.CSS_SELECTOR, 'body')
+    body.click()
+    for i in range(num):
+        body.send_keys(Keys.PAGE_DOWN)
+
+
+options = Options()
+options.add_experimental_option("detach", True)
+url = 'https://map.naver.com/v5/search'
+driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=options)
+driver.get(url)
+keyword = '강남역 한식'  # 검색어
+
+
+# 1. 선택자 찾을때까지 10초 대기
+time_wait(10, 'div.input_box > input.input_search')
+
+# 2. 검색어 입력 및 엔터
+search = driver.find_element(By.CSS_SELECTOR, 'div.input_box > input.input_search')
+search.send_keys(keyword)
+search.send_keys(Keys.ENTER)
+sleep(1)
+
+# 3. frame 변경 및 스크롤 다운
+switch_frame('searchIframe')
+page_down(50)
+sleep(3)
+
+# 4. 페이지 소스에서 가게 리스트 추출
+data = BeautifulSoup(driver.page_source, 'html.parser').select('div.CHC5F')
+columns = [('spot_name', pl.Utf8), ('rating_amt', pl.Float64), ('review_cnt', pl.Int64)]
+df = pl.DataFrame({}, schema=columns)
+for i, x in enumerate(data):
+    new = dict()
+    # 가게 이름, 별점, 리뷰 수 추출
+    new['spot_name'] = x.select_one('span.TYaxT').get_text()
+    raw = x.select_one('div.MVx6e').get_text() # 오늘 휴무별점4.29리뷰 999+
+    star, review = re.findall(r'별점(\d+\.\d+|\d+)', raw), re.findall(r'리뷰 (\d*)', raw)
+    new['rating_amt'] = float(star[0]) if star else float(-1)
+    new['review_cnt'] = int(review[0]) if review else -1
+    df = df.vstack(pl.DataFrame(new))
+
+
+today = datetime.now().strftime("%Y%m%d")
+df.write_csv(f'naver_{today}.csv')
